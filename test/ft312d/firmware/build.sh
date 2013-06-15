01ec2df8ea40c74d02618d54454da864c716edd5
Joe Ferner <joe@fernsroth.com>
2013-06-15 14:50:29 -0400

    add upload to build

--- test/ft312d/firmware/build.sh
+++ test/ft312d/firmware/build.sh
@@ -3,7 +3,7 @@ mkdir -p build
 cd build
 rm stm32-ft312d-test.elf
 make stm32-ft312d-test.bin && \
-make stm32-ft312d-test.list
-#arm-none-eabi-objcopy -Obinary stm32-ft312d-test.elf stm32-ft312d-test.bin
-#arm-none-eabi-objdump -S stm32-ft312d-test.elf > stm32-ft312d-test.list
+make stm32-ft312d-test.list && \
+cd .. && \
+sudo /opt/arm-linaro-eabi-4.6/bin/openocd
 
