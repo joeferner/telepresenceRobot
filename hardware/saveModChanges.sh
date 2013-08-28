6242e23ce699e64cc5776805ad0a260c59dd547c
Joe Ferner <joe@fernsroth.com>
2013-08-27 23:16:31 -0400

    button

--- hardware/saveModChanges.sh
+++ hardware/saveModChanges.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-~/dev/node-kicad-tools/bin/kicad-split.js -i telepresenceRobotNew.mod -o ~/dev/kicad-library/mods/
-./updateModFromLibrary.sh
+~/dev/node-kicad-tools/bin/kicad-split.js -i telepresenceRobotNew.mod -o ~/dev/kicad-library/mods/ && ./updateModFromLibrary.sh
